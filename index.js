d8fc2ab6ac773a933c60783444f5f74a927f00eb
Alexandr <ak4raulov@yandex.ru>
2021-12-26 17:23:41 +0300

    констрактор для слайдера

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-function Dog(name) {
-	this.myName = name;
-	this.voise = function () {
-		console.log('Hey, my name is ' + this.myName)
-	}
-}
+let slider1 = new Slider();
+let slider2 = new Slider();
 
-let laika = new Dog('laika');
-let buldog = new Dog('buldog');
-let bobik = new Dog('bobik');
-
-buldog.voise();
-laika.voise();
-bobik.voise();
-
-
-
-
-//let slider1 = sliderFactory.getNewSlider();
-//let slider2 = sliderFactory.getNewSlider();
-
-//slider1.start('slider1');
-//slider2.start('slider2');
+slider1.start('slider1');
+slider2.start('slider2');
 
